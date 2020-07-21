73749cf502e03ffe489a19880385fa933928c4f6
Jerome Arrigoni <jeromearrigonidev@gmail.com>
2020-07-21 12:14:03 +0200

    Classes Panier et Ligne
    Création des classes paniers et lignes

--- src/fr/eni/papeterie/bo/Panier.java
+++ src/fr/eni/papeterie/bo/Panier.java
@@ -1,7 +1,43 @@
 package fr.eni.papeterie.bo;
 
+import java.util.List;
+
 public class Panier {
 
 	private float montant;
+	private List<Ligne> lignesPanier;
+
+	public Panier() {
+		super();
+	}
+
+	public float getMontant() {
+		return montant;
+	}
+
+	public Ligne getLigne(int index) {
+
+	}
+
+	public List<Ligne> getLignesPanier() {
+		return this.lignesPanier;
+	}
+
+	public void addLigne(Article article, int qte) {
+
+	}
+
+	public void updateLigne(int index, int newQte) {
+
+	}
+
+	public void removeLigne(int index) {
+
+	}
+
+	@Override
+	public String toString() {
+		return "Panier [montant=" + montant + ", lignesPanier=" + lignesPanier + "]";
+	}
 
 }
